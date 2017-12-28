3fc8a616b56400c7e0a2caea0fc2bc5dd264af0f
mdavid5 <mcdav3636@gmail.com>
2017-12-28 14:31:45 -0700

    added if else statement

---
+++ daily/simpleloops.php
@@ -0,0 +1,30 @@
+<?php
+
+$num = 1;
+
+//create a loop to echo abc 9 times
+while ($num<=9){
+    echo "abc ";
+    // increase num by 1
+    $num ++;
+}
+
+
+$num = 1;
+
+//create a loop to echo xyz 9 times
+while ($num<=9){
+    echo "xyz ";
+    // increase num by 1
+    $num ++;
+}
+
+$num = 1;
+
+//create a loop to echo num
+while ($num<=9){
+    echo "$num ";
+    // increase num by 1
+    $num ++;
+}
+?>
