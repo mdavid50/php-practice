db239d739a87fe6f7c124e81eae845913fc0220b
mdavid5 <mcdav3636@gmail.com>
2017-12-28 13:56:02 -0700

    added if else statement

---
+++ daily/ifelse.php
@@ -0,0 +1,13 @@
+<?php
+//create a variable of current month
+$date = date('F');
+
+// create if august else statement
+if($date == "August"){
+    echo "<p> Its August, so its really hot.</p>";
+}else{
+    echo "<p> Not August, so at least not in the peak of the heat</p>";
+}
+
+?>
+
