3074c616ad71ec9184a181a538444ab5119a48bf
mdavid5 <mcdav3636@gmail.com>
2017-12-28 08:15:24 -0700

    practice

--- daily/concatenation.php
+++ daily/concatenation.php
@@ -5,4 +5,20 @@ $around = "around";
 // return the variable with concatenation
 echo 'what goes ' . $around . ' comes ' . $around . '.';
 
+
+$whatsit = "Greg";
+echo "value is " . gettype($whatsit) . ".<br/>\n";
+
+$whatsit = 88.9;
+echo "value is " . gettype($whatsit) . ".<br/>\n";
+
+$whatsit = true;
+echo "value is " . gettype($whatsit) . ".<br/>\n";
+
+$whatsit = 4;
+echo "value is " . gettype($whatsit) . ".<br/>\n";
+
+$whatsit = null;
+echo "value is " . gettype($whatsit) . ".<br/>\n";
+
 ?>
