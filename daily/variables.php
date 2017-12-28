211ab31ac94a51fe157f421bc457c9061a153eaf
mdavid5 <mcdav3636@gmail.com>
2017-12-27 22:01:55 -0700

    arithmetic practice

---
+++ daily/variables.php
@@ -0,0 +1,36 @@
+<?php
+
+echo "Twinkle twinkle little star <br/>";
+
+$star = "star";
+$twinkle = "Twinkle";
+
+echo "$twinkle $twinkle little $star<br/>";
+
+$star = "rock";
+$twinkle = "cow time";
+
+echo "$twinkle $twinkle little $star<br/>"
+
+
+// arithmetic operators and variables
+$x = 10;
+$y = 7;
+
+$result = $x + $y;
+echo "$x + $y = $result<br/>";
+
+$result = $x - $y;
+echo "$x -$y = $result<br/>";
+
+$result = $x * $y;
+echo "$x * $y = $result<br/>";
+
+$result = $x / $y;
+echo"$x / $y = $result<br/>";
+
+$result = $x % $y;
+echo "$x % $y = $result<br/>";
+
+?>
+
