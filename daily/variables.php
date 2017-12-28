ae63e212304ad1f200c54c09f611b5411c0eb895
mdavid5 <mcdav3636@gmail.com>
2017-12-27 22:57:25 -0700

    arithmetic practice

--- daily/variables.php
+++ daily/variables.php
@@ -32,5 +32,27 @@ echo"$x / $y = $result<br/>";
 $result = $x % $y;
 echo "$x % $y = $result<br/>";
 
+
+$num = 8;
+echo "value is now $num";
+
+$num + = 2;
+echo "value is now $num";
+
+$num - = 4;
+echo "calue is now $num";
+
+$num * = 5;
+echo "value is now $num";
+
+$num / = 3;
+echo "value is now $num";
+
+$num ++;
+echo "Increment value by one. value is now $num";
+
+$num --;
+echo "Decrement value by one. value is now $num";
+
 ?>
 
