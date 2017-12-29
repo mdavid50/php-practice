7ca36b1e7e07d72e92a2290cd4b7a86435b73c29
mdavid5 <mcdav3636@gmail.com>
2017-12-28 19:29:25 -0700

    arrays

--- daily/arrays.php
+++ daily/arrays.php
@@ -8,11 +8,47 @@ $weather = array(
     "hail",
     "sleet",
     "snow",
-    "wind",
+    "wind"
 );
 
 echo "<p> We've seen all kinds of weather this month. At the beginning of the month, ";
 echo "we had $weather[5] and $weather[6]. Then came $weather[1] with a few $weather[2] ";
 echo "and some $weather[0]. At least we didn't get any $weather[3] or $weather[4].</p>";
+
+
+//create array
+$cities = array(
+    "Tokyo",
+    "Mexico City",
+    "New York City",
+    "Mumbai",
+    "Seoul",
+    "Shanghai",
+    "Lagos",
+    "Buenos Aires",
+    "Cairo",
+    "London"
+);
+
+foreach ($cities as $city){
+    echo "$city,";
+}
+
+sort($cities);
+
+echo "\n<ul>\n";
+foreach ($cities as $city){
+    echo "<li>$city</li>\n";
+}
+
+//add cities
+array_push($cities, "Las Angeles", "Calcutta", "Osaka", "Beijing");
+
+sort($cities);
+
+foreach ($cities as $city){
+    echo "<li>$city</li>\n";
+}
+
 
 ?>
