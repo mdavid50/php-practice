9c4127b3bb387f6fe46ac46c2e141a5ea911c63b
mdavid5 <mcdav3636@gmail.com>
2018-01-03 10:55:26 -0700

    functions with parameters

---
+++ daily/js-practice/functions.js
@@ -0,0 +1,27 @@
+function printAmount() {
+    console.log( amount.toFixed(2));
+}
+
+var amount = 99.99;
+
+printAmount();
+
+amount = amount * 2 ;
+
+printAmount();
+
+
+function  printAmount(amt) {
+    console.log( amt.toFixed(2));
+}
+
+function formatAmount() {
+    return "$" + amount.toFixed(2);
+}
+
+var amount = 99.99;
+
+printAmount(amount * 2);
+
+amount = formatAmount();
+console.log( amount );
---
+++ daily/js-practice/loops.js
@@ -0,0 +1,11 @@
+numOfCustomers = 3;
+
+while ( numOfCustomers > 0) {
+    console.log( "How may I help you?");
+
+    //help the customer...
+
+    numOfCustomers = numOfCustomers - 1;
+}
+
+
