32ac3d4a990722af28cadceab2e41da60bbf95ed
mdavid5 <mcdav3636@gmail.com>
2018-01-02 22:12:45 -0700

    moved content

--- daily/js-practice/simple.js
+++ daily/js-practice/simple.js
@@ -15,23 +15,4 @@ a = a *2;
 console.log( a );
 
 
-var amount = 99.99;
-// change amount * 2
-amount = amount * 2;
-//return
-console.log( amount );
-//make it human worthy (and explicitly coerse amount)
-amount = "$" + String( amount );
-//return
-console.log( amount );
 
-var TAX_RATE = 0.08; // 8% sales tax
-
-var amount = 99.99;
-
-amount = amount * 2;
-
-amount = amount + (amount* TAX_RATE);
-
-console.log( amount );
-console.log( amount. toFixed( 2 ) );
