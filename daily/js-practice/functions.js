752402dbf1874ffbe26e0bee1a3dad831c06d71e
mdavid5 <mcdav3636@gmail.com>
2018-01-03 11:12:37 -0700

    functions with parameters

--- daily/js-practice/functions.js
+++ daily/js-practice/functions.js
@@ -25,3 +25,19 @@ printAmount(amount * 2);
 
 amount = formatAmount();
 console.log( amount );
+
+const TAX_RATE = 0.08;
+
+function calculateFinalPurchaseAmount(amt) {
+    //add the tax rate to amount
+    amt = amt +(amt * TAX_RATE);
+
+    return amt;
+}
+//set amount price to...
+var amount = 99.99;
+
+amount = calculateFinalPurchaseAmount(amount);
+
+//return the amount with tax
+console.log( amount.toFixed(2));
