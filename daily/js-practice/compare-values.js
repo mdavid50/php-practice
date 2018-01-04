59dace334531c6702fa14df082e443d9b7496706
mdavid5 <mcdav3636@gmail.com>
2018-01-04 08:05:03 -0700

    compare values

---
+++ daily/js-practice/compare-values.js
@@ -0,0 +1,29 @@
+/**
+ * EXPLICIT COERCION
+ **/
+// set variables, one a string and one a number
+var a = "42";
+//explicit coercion to a num
+var b = Number(a);
+
+//return a
+a;
+
+// return b
+b;
+
+
+/**
+ *IMPLICIT COERCION
+ **/
+//set var a to string
+var a = "42";
+
+//implicit coercion to a num
+var b = a * 1;
+
+//return a
+a;
+
+// return b
+b;
