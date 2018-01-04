086bfb7921fea2756eb4db0a5871b5bd95c8f1f5
mdavid5 <mcdav3636@gmail.com>
2018-01-04 09:34:28 -0700

    variables

--- daily/js-practice/compare-values.js
+++ daily/js-practice/compare-values.js
@@ -39,6 +39,7 @@ b;
 
 /**
  * EQUALITY
+ * == allows for coercion, === does not allow coercion.
  **/
 
 //set var a to string 42
@@ -50,3 +51,22 @@ var b = 42;
 a == b; //will return true
 
 a ===b; //will return false
+
+
+
+/**
+ *INEQUALITY
+ * also known as relational comparison
+ * uses <,>,<=,>=
+ * can use strings (lexicographically aka alphabetically) and numeric values
+ **/
+
+var a = 41;
+var b = "42";
+var c = "43";
+
+a < b; //returns true because it coerces the string to a number
+b < c; //returns true
+
+
+
---
+++ daily/js-practice/variables.js
@@ -0,0 +1,16 @@
+// create a function of foo
+function foo() {
+    var a = 1;
+
+    //create a loop
+    if (a>= 1) {
+        let b = 2;
+
+        while ( b < 5 ) {
+            let c = b * 2;
+            b++;
+
+            console.log( a + c);
+        }
+    }
+}
