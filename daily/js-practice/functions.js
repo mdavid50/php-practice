c2cd3c64744a174d4693bf8617eeea21c081d7ff
mdavid5 <mcdav3636@gmail.com>
2018-01-04 13:07:42 -0700

    closure

--- daily/js-practice/functions.js
+++ daily/js-practice/functions.js
@@ -71,3 +71,60 @@ var foo = function (){
 var x = function bar() {
 
 }
+
+
+
+/**
+ *IMMEDIATELY INVOKED FUNCTION EXPRESSIONS (IIFEs)
+ * a way to execute a function. short hand ish
+ **/
+
+(function IIFE() {
+    console.log("hello!");
+})(); // <- this runs the function
+
+
+var a = 42;
+
+(function IIFE() {
+    var a = 10;
+    console.log( a );  // returns 10
+})();
+
+x;  // returns 42
+
+
+//having return values
+var x = (function IIFE() {
+    return 42;
+})();
+
+x; // returns 42
+
+
+/**
+ * CLOSURE
+ * closure basically says to remember and do an action on
+ **/
+
+function makeAdder(x) {
+//this makes an inner var of x
+
+//an inner function will then use the var x, it remembers it or has closure over it
+    function add(y) {
+        return y + x;
+    };
+
+    return add;
+}
+
+var plusOne = makeAdder(1);
+
+var plusTen = makeAdder(10);
+
+plusOne( 3 ); // remembers the x or (1) from plusOne and used ( 3 ) as y --= 4 --
+plusOne( 41 );// remembers the x or (1) from plusONe and used ( 41 ) as y --= 42--
+
+plusTen( 13 );// remembers the x or (10) from plusTen and used ( 13 ) as y --= 23--
+
+
