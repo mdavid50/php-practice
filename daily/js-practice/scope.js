132be39a4ef3a6e86f87b8e0dd3a894053df545e
mdavid5 <mcdav3636@gmail.com>
2018-01-16 14:02:14 -0700

    IIFE variation

--- daily/js-practice/scope.js
+++ daily/js-practice/scope.js
@@ -102,3 +102,13 @@ var a = 2;
 })();
 
 console.log( a ); // 2
+
+var a = 2;
+
+(function IIFE( global) {
+    var a = 3;
+    console.log(a);// 3
+    console.log(global.a); // 2
+})(window);
+
+console.log( a ); // 2
