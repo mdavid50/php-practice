b242487fbb04f6524b38c2661579c2e36b73c71c
mdavid5 <mcdav3636@gmail.com>
2018-01-29 09:16:05 -0700

    hoisting explanation

--- daily/js-practice/scope-hoisting.js
+++ daily/js-practice/scope-hoisting.js
@@ -16,3 +16,13 @@ console.log( a ); // will return undefined
 
 var a = 2;
 
+
+
+/*
+ * the egg (declaration) comes before the chicken ( assignment )
+ * functions and declarations are hoisted to the top of the code. they are compiled by the engine before execution
+ * of the code. it can be thought of as the functions and declarations are HOISTED to the top
+ */
+
+//
+
