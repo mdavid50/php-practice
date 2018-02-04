b770f67b1213112087abcba35ede0f3c81b88f08
mdavid5 <mcdav3636@gmail.com>
2018-02-04 09:18:38 -0700

    saying hello

--- daily/php-only/classes-objects-methods.php
+++ daily/php-only/classes-objects-methods.php
@@ -13,3 +13,7 @@ $user1->firstName = "John";
 $user1->lastName = "Doe";
 
 echo $user1->firstName . " " . $user1->lastName;
+
+$hello = $user1->hello();
+
+echo $hello . ", " . $user1->firstName . " " . $user1->lastName;
